38569f20bdb04472382f23bde9844a316c088440
Allen Greaves <allengreaves@microsoft.com>
2024-04-10 10:24:54 -0700

    add opc-plc-sim to aio

--- deploy/azure-iot-operations.tf
+++ deploy/azure-iot-operations.tf
@@ -5,3 +5,15 @@ module "aio_full" {
   resource_group_name = var.resource_group_name
   location            = var.location
 }
+
+module "opc_plc_sim" {
+  source = "github.com/azure-samples/azure-edge-extensions-aio-iac-terraform//deploy/modules/opc-plc-sim?ref=main"
+
+  depends_on = [
+    module.aio_full
+  ]
+
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
